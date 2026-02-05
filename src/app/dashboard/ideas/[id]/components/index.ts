dfeca6249f23d1265e72e2cd2460411bfd6e309a
francanete <francanetedev@gmail.com>
2026-02-05 10:51:38 +0000

    delete IdeaRoadmapStatus

--- src/app/dashboard/ideas/[id]/components/index.ts
+++ src/app/dashboard/ideas/[id]/components/index.ts
@@ -1,7 +1,6 @@
 export { IdeaHeader } from "./idea-header";
 export { IdeaVoteBox } from "./idea-vote-box";
 export { IdeaStatus } from "./idea-status";
-export { IdeaRoadmapStatus } from "./idea-roadmap-status";
 export { IdeaDescription } from "./idea-description";
 export { IdeaInternalNote } from "./idea-internal-note";
 export { IdeaPublicUpdate } from "./idea-public-update";
